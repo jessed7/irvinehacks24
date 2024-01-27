afc29c8258dbd909f79cbb7907b3619c654aad2a
Jesse David <114846268+jessed7@users.noreply.github.com>
2024-01-26 19:36:06 -0800

    Started working on card management

--- scripts/card_management.js
+++ scripts/card_management.js
@@ -1,14 +1,12 @@
+// This file contains the code to manage the retrieving, editing, and storing of flashcards
+
+/* 
+When database storing is set up, follow this logic:
+    1. If the player username is in the database, then place the flashcard data from the DB into the cards object
+    2. If the username is not in the database, create a new Map to represent the cards
+
+*/
+const cards = new Map();
 
 
-function loadCardToDB(card){
-    //this takes a card object and will upload it to MongoDB
-}
-
-
-
-
-
-
-
-
-
+// Before starting the battle portion of the game, save the current card set into the database for the player
