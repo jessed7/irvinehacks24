5d9f8973ec12670c59c17f608f06311ba53dd513
Elliot Crowder <elliot.k.crowder@gmail.com>
2024-01-26 19:43:37 -0800

    answerValidation

--- scripts/game_logic.js
+++ scripts/game_logic.js
@@ -1,1 +1,36 @@
-const a = 3
+function validateAnswer(Useranswer,id,question){
+    //answer:string
+    //id:int
+    //question:map
+    var question = questionMap.get(id)
+    if(answer===question.answer){
+        question.numRight +=1
+        return true
+    }else{
+        question.numWrong +=1
+        return false
+
+    }
+}
+
+
+
+
+
+
+
+
+
+/*
+var question = {
+id:1
+answer:boo
+questionText:question
+numRight:0
+numWrong:0
+
+}
+
+
+*/
+
