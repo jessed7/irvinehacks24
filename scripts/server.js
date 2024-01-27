cc92a54f404cddfc1e3107bdb499d4a9c6743c51
Nathan La <nala@uci.edu>
2024-01-27 14:11:03 -0800

    making back end and created schema to interact with mongodb

--- scripts/server.js
+++ scripts/server.js
@@ -1,31 +1,58 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const User = require("./models/user");
 
-app.use(express.json())
+app.use(express.json());
 
 const URI =
-  "mongodb+srv://nala:FQ4bPg3XnqV2P5SR@cluster0.qr5q198.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://nala:FQ4bPg3XnqV2P5SR@cluster0.qr5q198.mongodb.net/CardData?retryWrites=true&w=majority";
 
 mongoose
   .connect(URI)
   .then((result) =>
-    app.listen(8000, () => {
-      console.log("Server Started on port 8000");
+    app.listen(5000, () => {
+      console.log("Server Started on port 5000, connected to MongoDB");
     })
   )
   .catch((err) => console.log(err));
 
-app.push('/add-cards', (req, res) => {
-  
-  try{
-    const card = await
+app.get("/get-user-cards", async (req, res) => {});
 
-  }catch{
+app.post("/backup-cards", async (req, res) => {
+  //this will try to update the cards list for the user given a json
+  const userID = req.body.user;
+  console.log(userID);
+  try {
+    if (User.exists({ user: userID })) {
+      console.log('user has been found');
+      let userData = await User.findOne({ user: userID })
+        .then((docs) => {
+          console.log("Result :", docs);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
 
+        userData.items = req.body.items;
+        userData.cardCount = req.body.cardCount;
+        userData.cardsAnswered = req.body.cardsAnswered;
+
+      const user = await userData.save()
+        .save()
+        .then((docs) => {
+          console.log("Saved These Values:", docs);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      const user = await User.create(req.body);
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
   }
-
-}
-);
-
-
+});
